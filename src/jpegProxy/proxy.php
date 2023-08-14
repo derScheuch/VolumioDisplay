0504d8f2ed06e6a899ad10e4e596d3cffe4a517e
Oliver Scheuch <oliver.scheuch1.partner@wintershalldea.com>
2023-08-14 17:49:18 +0200

    added some comments

--- src/jpegProxy/proxy.php
+++ src/jpegProxy/proxy.php
@@ -3,15 +3,20 @@
 if (isset($_GET['url'])) {
     $imageUrl = $_GET['url'];
    
-
+    // volumio sends the the albumart URLs either as a full quylified URL or as
+    // a relative volumio based URL
+    // format the given URL as needed 
     if (substr_compare($imageUrl, "http", 0, 4) === 0 ) {  
+        // for any reasons my php-setup does not allow to use https... simple hack
+        // until I find out how to make this possible
         if (substr_compare($imageUrl, "https", 0, 5) === 0 ) {
             $remote_image = file_get_contents( "http" .substr($imageUrl,5 ));
         } else {
             $remote_image = file_get_contents( $imageUrl);
         }
     } else {
-        $remote_image = file_get_contents("http://192.168.178.50" . $imageUrl);
+        // Please change this base volumio URL to your address
+        $remote_image = file_get_contents("http://volumio.local" . $imageUrl);
     }
     file_put_contents("/tmp/remote_image.jpg", $remote_image);
     $image = new Imagick("/tmp/remote_image.jpg");
@@ -31,8 +36,7 @@ if (isset($_GET['url'])) {
 
     $rgbValues = [];
     $pixels = $image->exportImagePixels(0, 0, $width, $height, "RGB", Imagick::PIXEL_CHAR);
-    $binaryValue = pack('c*', ...$pixels);
-
+   
     $packed = "";
     foreach ( $pixels as $byte ) {
         $packed .= chr( $byte );
